9b244c315b495e0ea1cf5bf6cbcbb653242454d9
virefleau <virefleau@pc-3.home>
2021-12-01 15:08:05 +0100

    chiffre 5

---
+++ TD6/src/chiffre5/chiffre5_v1.java
@@ -0,0 +1,16 @@
+package chiffre5;
+
+public class chiffre5_v1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("  ___");
+		System.out.println(" |");
+		System.out.println(" |___");
+		System.out.println("     |");
+		System.out.println("  ___|");
+
+	}
+
+}
