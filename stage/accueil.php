be404fe405b34bd25ba13e634f91a31f4f64f88b
kyoto16354 <95357473+kyoto16354@users.noreply.github.com>
2021-12-01 14:58:24 +0100

    Add files via upload

---
+++ stage/accueil.php
@@ -0,0 +1,29 @@
+<link href="css.css" media="all" rel="stylesheet" type="text/css"/>
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+    <text> 
+ <form method=POST action="connection.php">
+                <h1>Connexion</h1>
+                
+                <label><b>Email</b></label>
+                <input type="text" placeholder="Entrer l'adresse email" name="email" required>
+
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+<input type="submit" id='submit' value='LOGIN' >
+
+</text> 
+
+
+
+            </form>
+        </div>
+    </body>
+</html>
